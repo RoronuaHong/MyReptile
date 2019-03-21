caefb58ed938b88e66cf19864e66a10b6ebedcca
RoronuaHong <807672412@qq.com>
2019-03-22 00:23:19 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -9,50 +9,54 @@ const app = express()
 charset(superagent)
  
 app.all('/', (req, res, next) => {
-    // superagent.get('https://www.xiaoheihe.cn/community/57743/list')
-    //     .buffer(true)
-    //     .end((err, sres) => {
-    //         //常用的错误处理
-    //         if(err) {
-    //             return next(err) 
-    //         }
+    superagent.get('https://www.lagou.com/jobs/5719400.html')
+        .buffer(true)
+        .end((err, sres) => {
+            //常用的错误处理
+            if (err) {
+                return next(err)
+            }
 
-    //         let $ = cheerio.load(sres.text, { decodeEntities: false })
-    //         let items = []
+            let $ = cheerio.load(sres.text, { decodeEntities: false })
+            let items = []
+            let jobDetail = ''
+            
+            $('.job-detail p').each((idx, ele) => {
+                let $ele = $(ele)
 
-    //         $('.posts-list-wrapper .title').each((idx, ele) => {
-    //             let $ele = $(ele)
+                jobDetail += $ele.html()
+            })
+ 
+            items.push({
+                jobDetail,
+                companyName: $('.job_company_content .fl-cn').html()
+            })
 
-    //             items.push({
-    //                 title: $ele.(), 
-    //                 href: $ele.prop('href')
-    //             })
-    //         })
-
-    //         res.send(items)
-    //     })
-
-    request({
-        uri: 'https://api.xiaoheihe.cn/bbs/web/link/list',
-        method: 'GET',
-        qs: {
-            limit: 100,
-            offset: 60,
-            topic_id: 57743,
-            sort_filter: 'reply',
-            type_filter: 'all',
-            os_type: 'web',
-            version: '999.0.0',
-            hkey: '9550e3ac281d82d16ae953279df13823',
-            _time: 1553149609
-        }
-        // body: JSON.stringify({ email: '155com', password: 'cfdsd.' }) //账号密码 
-    }, (err, sres, data) => {
-        if(!err && sres.statusCode == 200) {
-            res.send(data)
-        }
+            res.send(items)
+        })
     })
-})
+
+    // request({
+    //     uri: 'https://api.xiaoheihe.cn/bbs/web/link/list',
+    //     method: 'GET',
+    //     qs: {
+    //         limit: 100,
+    //         offset: 60,
+    //         topic_id: 57743,
+    //         sort_filter: 'reply',
+    //         type_filter: 'all',
+    //         os_type: 'web',
+    //         version: '999.0.0',
+    //         hkey: '9550e3ac281d82d16ae953279df13823',
+    //         _time: 1553149609
+    //     }
+    //     // body: JSON.stringify({ email: '155com', password: 'cfdsd.' }) //账号密码 
+    // }, (err, sres, data) => {
+    //     if(!err && sres.statusCode == 200) {
+    //         res.send(data)
+    //     }
+    // })
+// })
 
 app.listen(3005, () => {
     console.log('app is listening at porning 3005.')
--- src/recruit/index.js
+++ src/recruit/index.js
@@ -1,7 +1,12 @@
 const express = require(`express`)
 const request = require(`request`)
+const cheerio = require(`cheerio`)
+const superagent = require(`superagent`)
+const charset = require(`superagent-charset`)
 
 const app = express()
+
+charset(superagent)
 
 app.all(`/`, (req, res, next) => {
     const post = `react`
@@ -16,12 +21,12 @@ app.all(`/`, (req, res, next) => {
             request({
                 url: url_parse,
                 method: `POST`,
-                headers: {
-                    Host: `www.lagou.com`,
-                    Origin: `https://www.lagou.com`,
-                    Referer: `https://www.lagou.com/jobs/list_${post}?labelWords=&fromSearch=true&suginput=?labelWords=hot`,
-                    Cookie: sres.headers[`set-cookie`]
-                },  
+                // headers: {
+                //     Host: `www.lagou.com`,
+                //     Origin: `https://www.lagou.com`,
+                //     Referer: `https://www.lagou.com/jobs/list_${post}?labelWords=&fromSearch=true&suginput=?labelWords=hot`,
+                //     Cookie: sres.headers[`set-cookie`]
+                // },  
                 qs: {
                     city: `上海`,
                     needAddtionalResult: false,
@@ -29,8 +34,43 @@ app.all(`/`, (req, res, next) => {
                     pn: 10,
                     kd: `${post}`
                 }
-            }, (err, sres, datas) => {
-                if(!err && sres.statusCode == 200) {
+            }, (err, tres, datas) => {
+                if(!err && tres.statusCode == 200) {
+                    let items = []
+                    let positionArr = []
+                    const dataArray = JSON.parse(datas).content ? JSON.parse(datas).content.positionResult.result : []
+
+                    dataArray.forEach(l => {
+                        positionArr = [...positionArr, `https://www.lagou.com/jobs/${l.positionId}.html`]
+                    })
+
+                    //TODO: 无法将内部内容输出到外部
+                    let promises = positionArr.map(item => {
+                        return new Promise(resolve => superagent.get(`https://www.lagou.com/jobs/${item}.html`)
+                            .buffer(true)
+                            .end((err, fres) => {
+                                //常用的错误处理 
+                                if(err) {
+                                    return next(err)
+                                }
+
+                                let $ = cheerio.load(fres.text, { decodeEntities: false })
+                                let jobDetail = ``
+
+                                $(`.job-detail p`).each((idx, ele) => jobDetail += $(ele).html())
+
+                                items.push({
+                                    jobDetail,
+                                    companyName: $(`.job_company_content .fl-cn`).html()
+                                })
+
+                                resolve(items) 
+                            }))
+                    })
+
+                    // Promise.all(promises).then(data => {
+                    //     res.send(items)
+                    // })
                     res.send(datas)
                 }
             })
